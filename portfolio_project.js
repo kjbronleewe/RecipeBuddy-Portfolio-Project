e1c271752a690ce8acfa204f0ef4f01110b2e1a7
Kelly Bronleewe <kjbronleewe@gmail.com>
2025-01-03 16:14:22 -0800

    updated javascript and css, added links for recipes

--- portfolio_project.js
+++ portfolio_project.js
@@ -1,43 +1,60 @@
-const searchBtn=document.querySelector('#search');
-searchBtn.addEventListener('click', fetchData);
+document.querySelector('#searchForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  console.log('form submitted');
+  fetchData();
+});
 
 async function fetchData() {
-    try{
-        let searchInputTxt=document.querySelector("#search-bar");
-        let ingredient= searchInputTxt.value.toLowerCase();
-        let response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
-        if(!response.ok){
-            throw new Error(`could not fetch resource`);
-        }
-        let data= await response.json();
-        console.log(data.meals);
-        console.log(data.meals[0].strMeal);
-        displayMeals(data.meals);
+  try {
+    let searchInputTxt = document.querySelector('#search-bar');
+    let ingredient = searchInputTxt.value.toLowerCase();
+    let response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${ingredient}`
+    );
+    if (!response.ok) {
+      throw new Error(`could not fetch resource`);
     }
-    catch(error){
-        console.error(error)
+    let data = await response.json();
+    if (data.meals) {
+      console.log(data.meals);
+      displayMeals(data.meals);
+    } else {
+      console.log('No meals found');
     }
-    
+    searchInputTxt.value = '';
+  } catch (error) {
+    console.error(error);
+  }
 }
-const cardDiv=document.querySelector('#card-container');
-const descriptionDiv=document.querySelector('#description');
 
-function displayMeals(meals){
-    descriptionDiv.remove(descriptionDiv.firstElementChild);
- 
-    for (let meal of meals){
-        let mealDiv=document.createElement('div');
-        cardDiv.appendChild(mealDiv);
+const cardDiv = document.querySelector('#card-container');
+const descriptionDiv = document.querySelector('#description');
 
-        let title=document.createElement('h5');
-        title.textContent=meal.strMeal;
-        mealDiv.appendChild(title);
+function displayMeals(meals) {
+  descriptionDiv.remove(descriptionDiv.firstElementChild);
+  cardDiv.innerHTML = '';
 
-        let image=document.createElement('img');
-        image.src=meal.strMealThumb;
-        image.width=250;
-        image.style.borderRadius=20 + 'px';
-        mealDiv.appendChild(image);
+  for (let meal of meals) {
+    let mealDiv = document.createElement('div');
+    mealDiv.className = 'meal-div';
+    cardDiv.appendChild(mealDiv);
 
-    }
+    let title = document.createElement('h5');
+    title.textContent = meal.strMeal;
+    title.className = 'meal-title'; 
+    mealDiv.appendChild(title);
+
+    let image = document.createElement('img');
+    image.src = meal.strMealThumb;
+    image.width = 250;
+    image.style.borderRadius = 20 + 'px';
+    mealDiv.appendChild(image);
+
+    let getRecipe = document.createElement('a');
+    getRecipe.setAttribute('href', meal.strSource);
+    getRecipe.setAttribute('target', '_blank');
+    getRecipe.textContent = 'Get the Recipe';
+    getRecipe.className = 'recipe-link';
+    mealDiv.appendChild(getRecipe);
+  }
 }
